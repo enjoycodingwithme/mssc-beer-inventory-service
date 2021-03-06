b846124f1193f77028957be16416ebf96dee6e01
enjoycoding <hasaraha@yahoo.de>
2021-03-06 22:29:26 +0100

    adding server config clinet configuration

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-23T21:02:35+0100",
+    date = "2021-03-06T22:09:02+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
